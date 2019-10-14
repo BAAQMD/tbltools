18b5046eaa10e2f70915d8b9af212e5d556f47aa
David Holstius <david.holstius@gmail.com>
2019-10-14 08:48:07 -0700

    add update.list(), found in inventory package

---
+++ R/update.list.R
@@ -0,0 +1,14 @@
+#' Update the value of items in a list (possibly adding new items).
+#'
+#' @param	object	a list
+#' @param	...		arguments of the form key=value
+#' @return	a copy of the list with updated items
+#'
+#' @export
+update.list <- function (object, ...) {
+  args <- list(...)
+  for (key in names(args)) {
+    object[[key]] <- args[[key]]
+  }
+  return(object)
+}
