e7f99af90f4aa7e9b1ee55e1c0c9755f299d8cda
David Holstius <david.holstius@gmail.com>
2020-04-27 09:24:37 -0700

    remove require() from test for pull_distinct()

--- tests/testthat/test-pull_distinct.R
+++ tests/testthat/test-pull_distinct.R
@@ -1,12 +1,12 @@
 context("pull_distinct")
 
-require(tidyverse)
-
 test_that("mtcars", {
 
-  mtcars %>%
-    pull_distinct(cyl) %>%
-    expect_setequal(
-      c(4, 6, 8))
+  expected <-
+    c(4, 6, 8)
+
+  expect_setequal(
+    pull_distinct(mtcars, cyl),
+    expected)
 
 })
