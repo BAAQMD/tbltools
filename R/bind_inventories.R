e5fdc23ff875a2b4ecbd79042d1c991c1306cd8c
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-04-06 15:25:34 -0700

    Remove many of the "no visible binding for global variable" R CMD CHECK errors for function bind_inventories.

--- R/bind_inventories.R
+++ R/bind_inventories.R
@@ -7,6 +7,11 @@
 #' @param verbose display messages
 #'
 #' @export
+#' @importFrom magrittr %>%
+#' @importFrom purrr map
+#' @importFrom funtools %not_in% all_same
+#' @importFrom dplyr mutate na_if mutate_at bind_rows coalesce vars
+#' @importFrom forcats fct_inorder
 bind_inventories <- function (
   ...,
   .id = "inventory",
