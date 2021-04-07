b50d03cc5037cd3575a1b8704d6bd5dc05f91004
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-04-07 14:28:58 -0700

    Add more @importFrom to functions.

--- R/sort_by.R
+++ R/sort_by.R
@@ -1,6 +1,8 @@
 #' Sort by a summary (statistic) of a (grouping) variable
 #'
 #'  @export
+#'  @importFrom dplyr group_by_ desc
+#'  @importForm stats median
 sort_by_ <- function (input_data, wt_var, group_var, wt_fun = median, na.rm = TRUE) {
   grouped <- group_by_(input_data, group_var)
   ranked <- mutate(grouped, .wt = apply(get(wt_var), 1, wt_fun, na.rm = na.rm))
