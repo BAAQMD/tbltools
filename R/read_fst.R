5570f8c67cf8bbdcfcc41bec6fdc0ef5a57847ea
David Holstius <david.holstius@gmail.com>
2021-04-09 09:05:35 -0700

    support read_fst(..., verbose = TRUE)

--- R/read_fst.R
+++ R/read_fst.R
@@ -5,8 +5,12 @@
 #' Returns a tibble instead of a data.frame.
 #'
 #' @export
-read_fst <- function (...) {
+read_fst <- function (
+  ...,
+  verbose = getOption("verbose", default = FALSE)
+) {
 
+  msg <- function (...) if(isTRUE(verbose)) message("[read_fst] ", ...)
   fst_data <- fst::read_fst(...)
   return(tibble::as_tibble(fst_data))
 
