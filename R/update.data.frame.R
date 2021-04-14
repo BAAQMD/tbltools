53191abb78f18ac26d4f58d8d7fc4d6e2e58fe6b
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-04-14 14:36:26 -0700

    Fix R CMD CHECK warning "  'library' or 'require' calls to packages already attached by Depends:   ... ‘lazyeval’ "
    Also, add relevant @importFrom directives for this function.

--- R/update.data.frame.R
+++ R/update.data.frame.R
@@ -3,9 +3,11 @@
 #' Analogous to SQL UPDATE command
 #'
 #' @export
+#' @importFrom lazyeval lazy_eval expr_text
+#' @importFrom purrr reduce
+#' @importFrom dplyr mutate bind_rows
 update.data.frame <- function (input_data, .where, ..., verbose = getOption("verbose")) {
 
-  require(lazyeval)
   msg <- function (...) if(isTRUE(verbose)) message("[update.data.frame] ", ...)
 
   # Logical union of all conditions
