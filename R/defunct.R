034cf93f61c274f87c618942fd33e3ed9ca769d9
David Holstius <david.holstius@gmail.com>
2021-04-15 10:16:47 -0700

    make sort_by_() defunct no occurrences of "sort_by_" in any .R files in inventoryverse pkgs that I can find

--- R/defunct.R
+++ R/defunct.R
@@ -4,6 +4,7 @@
 #'  @importFrom dplyr group_by_ desc
 #'  @importForm stats median
 sort_by_ <- function (input_data, wt_var, group_var, wt_fun = median, na.rm = TRUE) {
+  .Defunct()
   grouped <- dplyr::group_by_(input_data, group_var)
   ranked <- mutate(grouped, .wt = apply(get(wt_var), 1, wt_fun, na.rm = na.rm))
   arrange(ranked, dplyr::desc(.wt))
