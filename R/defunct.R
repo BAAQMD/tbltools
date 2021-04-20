e64cd8d946e73875ab597edd40bcc802b4c111d6
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-04-20 15:47:43 -0700

    Move sort_by to defunct.

--- R/defunct.R
+++ R/defunct.R
@@ -109,3 +109,17 @@ print_tbl <- function (
     ...)
 
 }
+
+#' Sort by a summary (statistic) of a (grouping) variable
+#'
+#'  @export
+#'  @importFrom dplyr group_by_ desc
+#'  @importForm stats median
+sort_by_ <- function (input_data, wt_var, group_var, wt_fun = median, na.rm = TRUE) {
+  .Defunct()
+
+  grouped <- dplyr::group_by_(input_data, group_var)
+  ranked <- mutate(grouped, .wt = apply(get(wt_var), 1, wt_fun, na.rm = na.rm))
+  arrange(ranked, dplyr::desc(.wt))
+}
+
