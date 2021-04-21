a835c757d91ceea92666cd97e74fb5e1dcd1a1e4
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-04-20 17:07:37 -0700

    Replace dplyr::group_by_at call with dplyr::across. Done because the documentation for ?group_by_at now says that the function has been "superseded" by ?across.
    This change is somewhat pedantic, because the only function in the package that uses this function is .Defunct().
    
    Also, I am not 100% sure how to test this function.

--- R/defunct.R
+++ R/defunct.R
@@ -113,13 +113,13 @@ print_tbl <- function (
 #' Sort by a summary (statistic) of a (grouping) variable
 #'
 #'  @export
-#'  @importFrom dplyr group_by_ desc
+#'  @importFrom dplyr mutate desc across
 #'  @importForm stats median
 sort_by_ <- function (input_data, wt_var, group_var, wt_fun = median, na.rm = TRUE) {
   .Defunct()
 
-  grouped <- dplyr::group_by_(input_data, group_var)
-  ranked <- mutate(grouped, .wt = apply(get(wt_var), 1, wt_fun, na.rm = na.rm))
+  ranked <- input_data %>%
+    dplyr::mutate(dplyr::across(!!group_var), .wt = apply(get(wt_var), 1, wt_fun, na.rm = na.rm))
   arrange(ranked, dplyr::desc(.wt))
 }
 
