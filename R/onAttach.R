e704881ff7ef5dbbaa0e42907538c0fcdac8572a
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-06-04 13:40:54 -0700

    Add in write_csv to conflicts.

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,6 @@
 #' @importFrom conflicted conflict_prefer
 .onAttach = function(...)
 {
-  conflict_prefer("read_csv", "tbltools")
+  conflict_prefer("read_csv", "tbltools", "readr")
+  conflict_prefer("write_csv", "tbltools", "readr")
 }
