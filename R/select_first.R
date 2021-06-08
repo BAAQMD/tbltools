10a339ed5e5c0fa91db5dd6e8d7bae48bfced1cc
David Holstius <david.holstius@gmail.com>
2021-06-08 09:50:02 -0700

    fix select_last() - select_first() didn't seem to suffer from the same bug - rewrote select_first() anyway, for symmetry

--- R/select_first.R
+++ R/select_first.R
@@ -1,21 +1,19 @@
-#' Move selected column(s) to first position
+#' Move selected column(s) to first position(s)
 #'
 #' @param input_data tabular data
 #' @param ... column name(s)
+#'
+#' @importFrom tidyselect vars_select all_of
+#' @importFrom dplyr select
 #'
 #' @seealso [select_last()]
 #'
 #' @return
 #' @export
 #'
-select_first <- function (input_data, ...) {
-
-  selected_vars <-
-    names(select(input_data, ...))
-
-  select(
-    input_data,
-    all_of(selected_vars),
-    everything())
-
+select_first <- function (input_data, ...)  {
+  selected_vars <- tidyselect::vars_select(names(input_data), ...)
+  other_vars <- setdiff(names(input_data), selected_vars)
+  selected_data <- dplyr::select(input_data, tidyselect::all_of(selected_vars), tidyselect::all_of(other_vars))
+  return(selected_data)
 }
