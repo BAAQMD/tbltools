7f043040a1127b3aa1c9aa021a856271efd8c163
Ari Lamstein <ari@baaqmd-aim-rstudio.ez42qaz01t4ezoqptw4txvquxh.dx.internal.cloudapp.net>
2021-06-14 14:55:00 -0700

    Conflict resolution should be for "read_fst", not "fst".
    Put .onAttach into zzz.R for sake of convention.

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,5 @@
 {
   conflict_prefer("read_csv", "tbltools", "readr")
   conflict_prefer("write_csv", "tbltools", "readr")
-  conflict_prefer("fst", "tbltools", "fst")
+  conflict_prefer("read_fst", "tbltools", "fst")
 }
