9cb94001c23dadf32ec1feb66d7f8ba96c37bbda
Yuan Du <38868682+ydu0316@users.noreply.github.com>
2021-06-21 14:13:13 -0700

    Update zzz.R
    Fix typo of `dplyer` to `dplyr`.

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,5 @@
   conflict_prefer("read_csv", "tbltools", "readr")
   conflict_prefer("write_csv", "tbltools", "readr")
   conflict_prefer("read_fst", "tbltools", "fst")
-  conflict_prefer("filter", "dplyer", "stats")
+  conflict_prefer("filter", "dplyr", "stats")
 }
